d44d0bd4c91b1f4a8830a4e6f0cee4fa3620bec0
Melissa Nock <melnock@gmail.com>
2017-12-21 14:18:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def badge_maker (name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (attendees)
+  attendees.map {|x| "Hello, my name is #{x}."}
+end
+
+def assign_rooms (attendees)
+  assignments = []
+  i=0 
+  while assignments.size < attendees.size
+    assignments.push("Hello, #{attendees[i]}! You'll be assigned to room #{i + 1}!")
+    i+=1 
+  end
+  assignments
+end
+
+def printer(attendees)
+  i=0 
+ while i < attendees.size do
+   puts badge_maker (attendees[i])
+   i+=1 
+ end
+   i=0 
+ while i < attendees.size do
+   assign_rooms (attendees)
+   
+   i+=1 
+ end
+  
+end
